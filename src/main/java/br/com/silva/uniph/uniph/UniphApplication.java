3e108215c34f2150d6ae4a82a3b17cc090b137ac
Danilo da Silva Pereira <danilo.pereira@basis.com.br>
2018-10-24 17:13:16 -0300

    First commit :smile:

---
+++ src/main/java/br/com/silva/uniph/uniph/UniphApplication.java
@@ -0,0 +1,12 @@
+package br.com.silva.uniph.uniph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniphApplication.class, args);
+	}
+}
